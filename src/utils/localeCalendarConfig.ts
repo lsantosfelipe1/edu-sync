3a93ab0833c2519d17891925a7f60aa6c0fa90fb
lsantosfelipe1 <fsl1@aluno.ifnmg.edu.br>
2025-02-23 17:49:18 -0300

    Update localeCalendarConfig.ts

--- src/utils/localeCalendarConfig.ts
+++ src/utils/localeCalendarConfig.ts
@@ -3,14 +3,14 @@ export const ptBR = {
     'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
     'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
   ],
-  monthShortNames: [
+  monthNamesShort: [ 
     'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun',
     'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
   ],
   dayNames: [
     'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'
   ],
-  dayShortNames: [
+  dayNamesShort: [
     'D', 'S', 'T', 'Q', 'Q', 'S', 'S'
   ],
   today: 'Hoje'
